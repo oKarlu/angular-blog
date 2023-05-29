7e2a166dc96967693ad2f10fb1ed7d65adf07aa8
Carlos Marinho <cadumsantos@hotmail.com>
2023-05-29 07:28:44 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"0",
+    "photo": "https://sm.ign.com/ign_br/screenshot/default/homi-de-ferro_3qsx.jpg",
+    "title": "NEW IRON MAN",
+    "description": "Marvel broadcast new iron man film"
+  },
+  {
+    "id":"1",
+    "photo": "https://d17lbu6bbzbdc8.cloudfront.net/wp-content/uploads/2021/07/15150307/Loki-DOctor-Strange-2-Benedict-Cumberbatch-Tom-Hiddleston.jpg",
+    "title": "New serie disney+",
+    "description": "bla bla bla bla"
+  }
+]
